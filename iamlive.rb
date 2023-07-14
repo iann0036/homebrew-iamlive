bd4e1b4b6d5e699cec820b3c7672ef2e8829509d
Ian Mckay <contact@ianmckay.com.au>
2023-07-14 13:08:04 +1000

    v0.53.0

--- iamlive.rb
+++ iamlive.rb
@@ -4,16 +4,16 @@
 class Iamlive < Formula
   desc "Generate an IAM policy from AWS calls using client-side monitoring (CSM) or embedded proxy."
   homepage "https://github.com/iann0036/iamlive"
-  version "0.52.0"
+  version "0.53.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/iann0036/iamlive/releases/download/v0.52.0/iamlive-v0.52.0-darwin-amd64.zip"
-    sha256 "0c30dd0963d2d0a8f1bd7a71e629db945dcbadbca7ba6bceb525d92ac6b3c8b4"
+    url "https://github.com/iann0036/iamlive/releases/download/v0.53.0/iamlive-v0.53.0-darwin-amd64.zip"
+    sha256 "215d14c13ffa620c32defeb82969b31bb6d66a39793273d6c85ad78a24c2c24f"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/iann0036/iamlive/releases/download/v0.52.0/iamlive-v0.52.0-darwin-arm64.zip"
-    sha256 "dc09094487e24b77974397b78292d982084f6bd2b0c67fab9b405932b1ba2573"
+    url "https://github.com/iann0036/iamlive/releases/download/v0.53.0/iamlive-v0.53.0-darwin-arm64.zip"
+    sha256 "5815cbc5b950bc7c21b9c9f6d0411e6bb7ac8ae226920746a089c4237b70f174"
   end
 
   def install
