34b9bc8cbb84ca68228e75be5a4fba892b8939cb
Ian Mckay <contact@ianmckay.com.au>
2025-03-24 22:31:27 +1100

    v1.1.23

--- iamlive.rb
+++ iamlive.rb
@@ -4,28 +4,28 @@
 class Iamlive < Formula
   desc "Generate an IAM policy from AWS, Azure, or Google Cloud (GCP) calls using client-side monitoring (CSM) or embedded proxy."
   homepage "https://github.com/iann0036/iamlive"
-  version "1.1.22"
+  version "1.1.23"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.22/iamlive-v1.1.22-darwin-amd64.zip"
-    sha256 "16e9b4c6de7d611eb4a9c9b82c9b8a9398aa01e78a0427912e6caa36ebde1756"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.23/iamlive-v1.1.23-darwin-amd64.zip"
+    sha256 "556db8da3a3b69517fbc302caad6226170c7a016d175715c9d0a2e5aa2e3ad4a"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.22/iamlive-v1.1.22-darwin-arm64.zip"
-    sha256 "2dcd2e2d7f40ef34eb8015f334620ee8660cee1daa36cf18421c68d1e65f04ce"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.23/iamlive-v1.1.23-darwin-arm64.zip"
+    sha256 "3f4b8f8bd0ff143057a2c6901898c5b4b4d8b23f7ac022ec4b6653f109dc08a7"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.22/linux-v1.1.22-linux-386.tar.gz"
-    sha256 "59a8deba43fcef7226d6bdab2248b6ae7c519c2bff90d4a7db2b9a61458fcdf0"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.23/linux-v1.1.23-linux-386.tar.gz"
+    sha256 "3480fc2d1cd69d1010474d3debd16a000ed06fc78fa1a652415425498ee1fc1d"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.22/linux-v1.1.22-linux-amd64.tar.gz"
-    sha256 "0512ac4ea96fe3fe5365599cb21127e97a7063c22d9eaa6ee16b3bdf2cba8b3e"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.23/linux-v1.1.23-linux-amd64.tar.gz"
+    sha256 "bb0ed560705a8f5377e696dede9d6a0cbbbbbb62b8ff27a56ca1481fa48324f6"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.22/linux-v1.1.22-linux-arm64.tar.gz"
-    sha256 "7a14c6e594b2819e8f9b7ac0a7e14caeecd45252da227aa1e88fddde48dbf987"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.23/linux-v1.1.23-linux-arm64.tar.gz"
+    sha256 "0acb20f770c4c7c4dc2c30d910e161c1d7a0c1e7501b9c295ad54f5ce84bc161"
   end
 
   def install
