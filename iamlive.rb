3f00629d0219f1ceb812c6e9e9572874427654cb
Ian Mckay <contact@ianmckay.com.au>
2025-07-12 20:55:44 +1000

    v1.1.26

--- iamlive.rb
+++ iamlive.rb
@@ -4,28 +4,28 @@
 class Iamlive < Formula
   desc "Generate an IAM policy from AWS, Azure, or Google Cloud (GCP) calls using client-side monitoring (CSM) or embedded proxy."
   homepage "https://github.com/iann0036/iamlive"
-  version "1.1.25"
+  version "1.1.26"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.25/iamlive-v1.1.25-darwin-amd64.zip"
-    sha256 "c75729442583142e6ee25d18c36c1214f15fa51115ad90d845e093159e0c8574"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.26/iamlive-v1.1.26-darwin-amd64.zip"
+    sha256 "694274fa41a39a798990eb0ddba2443b693c5b51eb1b9145767fc665a09e2008"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.25/iamlive-v1.1.25-darwin-arm64.zip"
-    sha256 "3a68e587f03f97472071311f8f4ec849a5fd57da1a51837a3cb808df22d75af5"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.26/iamlive-v1.1.26-darwin-arm64.zip"
+    sha256 "9af848293310c75487a9128c6574b5ac0b7d57e7f798e9843a35409603fdb270"
   end
   if OS.linux? && Hardware::CPU.intel? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.25/iamlive-v1.1.25-linux-386.tar.gz"
-    sha256 "4aa12d4e1447a429e8e25ddf83f11112ff01992e88906e5d60911557775a6369"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.26/iamlive-v1.1.26-linux-386.tar.gz"
+    sha256 "d6310b809f6dcc539b1f4a962a5ab5f6cec1c94724af5075fae1931a1701ae29"
   end
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.25/iamlive-v1.1.25-linux-amd64.tar.gz"
-    sha256 "881c781bf6d9a231ee23e5158eb77ee823ba5bf06d3d48179147c5ed66f2994f"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.26/iamlive-v1.1.26-linux-amd64.tar.gz"
+    sha256 "4edf412742b62768232ef964482113aba49db3a2436d64b835abadddbacbf50b"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/iann0036/iamlive/releases/download/v1.1.25/iamlive-v1.1.25-linux-arm64.tar.gz"
-    sha256 "6a6c15de0c76373aeadc39667b88fa20620cb8f0c7b9cbd0b931d2f0df649fe8"
+    url "https://github.com/iann0036/iamlive/releases/download/v1.1.26/iamlive-v1.1.26-linux-arm64.tar.gz"
+    sha256 "5cd3b425cdecf8bb230b4a3feae551366ab8096682e314886b55738a74f916f7"
   end
 
   def install
